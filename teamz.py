881a925f4d79f5e22d63b733edcad17ae2c3b195
haydonS <57916478+haydonS@users.noreply.github.com>
2020-01-30 14:18:31 -0500

    Update teamz.py

--- teamz.py
+++ teamz.py
@@ -1,1 +1,14 @@
+from random import *
+
+
+
+roomArray = []
+for i in range(999):
+   roomArray.append(False)
+
+roomArray[505] = ("you have reached the workout room, here lies a treadmill for the astronauts to workout and stay fit /nto the west is the kitchen, to the north you hear creeking, south and east are the walls to you ship")
+roomArray[504] = ("this is the hallway... /neast you here snoring, to the west you hear a windex bottle, to the south you hear a treadmill") 
+roomArray[604] = ("to the north the snoring increases, to the east you hear a flush, to the west the windex bottle sound grows lighter and to the south you see a window looking down to the earth")
+roomArray[603] = ("you have now reached the bedrooms... there is many clues here to help you escape /nTo the south is the hallway and to the west, east, and north are walls")
+
 

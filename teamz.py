9662fc398f1da3c11fc16c56348afc14c7b2f9af
haydonS <57916478+haydonS@users.noreply.github.com>
2020-02-12 14:18:42 -0500

    Update teamz.py

--- teamz.py
+++ teamz.py
@@ -20,19 +20,27 @@ roomArray[304] = "You look out the window and see the beautiful planet Earth. Th
 roomArray[403] = "You enter the kitchen and check the sink. It seems there are some dishes that need to be put back..."
 roomArray[404] = "To your west is a window freshly cleaned. East is a long hall. North is the kitchen, and south is the dining room."
 roomArray[405] = "You walk into the dining room. It seems a fellow astronaut left something of theirs."
+roomArray[408] = "you see a man snorting something but he quickly hides it behind his back to you south, east, and west are walls."
+roomArray[407] = "you here the snorting sound grow louder from the south to your north and west are walls."
+
 
 #bedrooms
-roomArray[505] = "You have reached the workout room, here lies a treadmill for the astronauts to workout and stay fit \nto the west is the kitchen, to the north you hear creeking, south and east are the walls to you ship."
+roomArray[507] = "you see a running computer with a game window open to your west you faintly hear snorting to your north and south are walls."
+roomArray[505] = "You have reached the workout room, here lies a treadmill for the astronauts to workout and stay fit /nto the west is the kitchen, to the north you hear creeking, south and east are the walls to you ship."
 roomArray[504] = "This is the hallway... east you here snoring, to the west you hear a windex bottle, to the south you hear a treadmill."
 roomArray[604] = "To the north the snoring increases, to the east you hear a flush, to the west the windex bottle sound grows lighter and to the south you see a window looking down to the earth."
-roomArray[603] = "You have now reached the bedrooms... there is many clues here to help you escape \nTo the south is the hallway and to the west, east, and north are walls."
-
+roomArray[603] = "You have now reached the bedrooms... there is many clues here to help you escape /nTo the south is the hallway and to the west, east, and north are walls."
+roomArray[605] = "Here is the hallway that leads to the secod part of the station... \nto your east is the workout room and to your south you see the server room..."
+roomArray[606] = "You have reached the router room. To south you smell the sweet smell of deep fried, bagged chicken nuggets"
+roomArray[607] = "you have reached the secondary kitchen, to your south you see a mysterious locked room..."
+roomArray[608] = "this was a very secret room... you have reached YODA!!! congrats on nothing"
 #hallway
 roomArray[704] = "You smell something awful further down the hallway to the east, to the west you hear a quiet spraying sound."
+roomArray[707] = "you have reached the room of pizza... to your west you smell a great smell"
 roomArray[804] = "On the floor you see a towel with something brown on it, theres a door with a man and a women drawing on it to the south and walls to the east and north."
+roomArray[807] = "youve entered a room of nothingness... to your west you smell a delightful smell"
 roomArray[805] = "You see a flouding toilet the awful smell is coming from the toilet and there are brown moist mushy bits of feces you notice something in the toilet. to the east is a air sealed door."
 roomArray[905] = "Theres a door east it seems to be locked but you can see outside to outerspace."
-
 # Mr. Riley's map class v1.90208
 #
 # How to use Mr. Riley's map class...
